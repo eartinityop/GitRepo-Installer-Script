82220e51bf07bce73426b93906111bf6ffa808e4
Eartinity <77088825+eartinityop@users.noreply.github.com>
2024-02-25 16:58:10 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 mkdir -p ~/bin 
-curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo  
+sudo curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo  
 chmod a+x ~/bin/repo 
 export PATH="$HOME/bin:$PATH" 
 source ~/.bashrc
