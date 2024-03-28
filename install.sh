c24bb93f21eb9eddb635c7e941d6904b6376fd36
Eartinity <77088825+eartinityop@users.noreply.github.com>
2024-03-28 07:43:05 +0530

    Remove sudo input

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 mkdir -p ~/bin 
-sudo curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo  
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo  
 chmod a+x ~/bin/repo 
 export PATH="$HOME/bin:$PATH" 
 source ~/.bashrc
